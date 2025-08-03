80b6d2c4f12e6b0cebd8433df9dfbff95bda1bad
Lily <8723107+lily-gh@users.noreply.github.com>
2025-08-03 17:19:19 +0200

    wip: adding svelte UI

---
+++ webview-ui/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
+// https://vite.dev/config/
+export default defineConfig({
+    root: __dirname,
+    base: './',
+    build: {
+        outDir: path.resolve(__dirname, '../media'),
+        emptyOutDir: true,
+        target: 'esnext',
+        assetsDir: '',
+        rollupOptions: {
+            input: path.resolve(__dirname, 'index.html'),
+            output: {
+                manualChunks: undefined,
+            },
+        },
+    },
+    plugins: [svelte()],
+});
+//# sourceMappingURL=vite.config.js.map
